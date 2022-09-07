b1eb199abfbc11c8c47c5e16b5469541ed15c032
francisco insua <franinsua7@gmail.com>
2022-09-07 18:09:01 -0300

    taskInterface

---
+++ src/app/Task.ts
@@ -0,0 +1,7 @@
+export interface Task{
+    id:number;
+    title:string;
+    date:string;
+    hour:string;
+    reminder:boolean;
+}
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,33 @@
+import { Task } from "./Task"
+export const TASKS:Task[]=[
+    {
+        id:1,
+        title:"Ir al super",
+        date:"8 de septiembre",
+        hour:"18:00",
+        reminder:false
+    },
+    {
+        id:2,
+        title:"Examen matematicas",
+        date:"23 de septiembre",
+        hour:"08:00",
+        reminder:true
+    },
+    {
+        id:3,
+        title:"Cumple cata",
+        date:"10 de septiembre",
+        hour:"22:00",
+        reminder:false
+    },
+    {
+        id:3,
+        title:"Terminar proyecto final",
+        date:"04 de enero",
+        hour:"23:59",
+        reminder:true
+    }
+    
+]
+

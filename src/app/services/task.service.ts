72ecbd28698db43fab57882f3a53521b17fc9d22
francisco insua <franinsua7@gmail.com>
2022-09-09 15:55:13 -0300

    jsonServer

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { TASKS } from 'src/app/mock-tasks';
 import { Task } from 'src/app/Task';
@@ -8,10 +9,12 @@ import { Task } from 'src/app/Task';
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks';
 
-  constructor() { }
-  getTasks():Observable<Task[]>{
-    const tasks=of(TASKS);
-    return tasks;
+  constructor(
+    private http:HttpClient
+  ) { }
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
